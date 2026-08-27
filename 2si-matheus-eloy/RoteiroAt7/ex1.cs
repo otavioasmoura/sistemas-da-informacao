450f42669c26153b0774849a59e9d1b091a098dd
otavioasmoura <otavio207augusto@gmail.com>
2026-08-27 09:35:12 -0300

    terceiro

--- 2si-matheus-eloy/RoteiroAt7/ex1.cs
+++ 2si-matheus-eloy/RoteiroAt7/ex1.cs
@@ -7,13 +7,24 @@ class Ex1
         char[] decodificada = new char[codificada.Length];
         // implementar
         return new String(decodificada);
+        int j = 0;
+        for (int i = 0; i < codificada.Length; i++)
+        {
+            if (codificada[i] != 'p')
+            {
+                decodificada[j] = codificada[i];
+                j++;
+            }
+
+        }
     }
     static void Main()
     {
         String codficada;
         Console.Write("Entre com a frase codficada: ");
         codficada = Console.ReadLine();
-
+        // chamar a função
+        Console.WriteLine(decodificar(codficada));
 
     }
 }
