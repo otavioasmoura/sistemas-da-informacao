b9a496a492514b222d88e709101b2eaed1be4b11
otavioasmoura <otavio207augusto@gmail.com>
2026-08-27 09:24:26 -0300

    dois

--- 2si-matheus-eloy/RoteiroAt7/ex1.cs
+++ 2si-matheus-eloy/RoteiroAt7/ex1.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 
-Class Ex1()
+class Ex1
 {
+    static String decodificar(String codificada)
+    {
+        char[] decodificada = new char[codificada.Length];
+        // implementar
+        return new String(decodificada);
+    }
     static void Main()
     {
         String codficada;
-        Console.Write("Entre com a frase codficada: ")
+        Console.Write("Entre com a frase codficada: ");
         codficada = Console.ReadLine();
 
-    
+
     }
 }
