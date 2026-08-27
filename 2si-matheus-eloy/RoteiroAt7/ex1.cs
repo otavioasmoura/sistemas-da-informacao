261bd52b9e976ed0214f28c6f92ae63fdd07b84b
otavioasmoura <otavio207augusto@gmail.com>
2026-08-27 09:14:51 -0300

    aula2708

---
+++ 2si-matheus-eloy/RoteiroAt7/ex1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Class Ex1()
+{
+    static void Main()
+    {
+        String codficada;
+        Console.Write("Entre com a frase codficada: ")
+        codficada = Console.ReadLine();
+
+    
+    }
+}
